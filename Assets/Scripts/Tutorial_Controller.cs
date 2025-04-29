ed845bf7eb7efa516fc319146b43b7f1c1d545fd
Arian-G <arian.goudar@mavs.uta.edu>
2025-04-29 13:20:56 -0500

    Update LA3 tilemap

---
+++ Assets/Scripts/Tutorial_Controller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Tutorial_Controller : MonoBehaviour
+{
+    // Tutorial Controller
+    // Handles tutorial section and
+    // triggers assoc. popups.
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
