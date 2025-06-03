07fd9d1752266021030a5bc8407ec9987090391f
Preston Loera <prl9535@mavs.uta.edu>
2025-06-03 14:03:37 -0500

    Added camera shake

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -1,18 +1,26 @@
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Camera_Controller : MonoBehaviour
 {
     [Header("Camera Controller Misc.")]
     public bool inDungeon = false;
-
     public static Camera_Controller instance;
+    public CinemachineBrain brain;
+
+    [Header("Camera Shake")]
+    public AnimationCurve cameraShakeCurve;
+    public float cameraShakeDuration = 1f;
+    public bool isShaking;
+    public bool cameraShakeEnabled = true;
+
 
     private Transform _playerTransform;
 
     private void Awake()
     {
-        if(!instance)
+        if (!instance)
         {
             instance = this;
         }
@@ -26,18 +34,54 @@ public class Camera_Controller : MonoBehaviour
     {
         _playerTransform = Player_Controller.instance.transform;
         inDungeon = false;
-        //transform.position = _playerTransform.position;
+        transform.position = _playerTransform.position;
+        brain = GetComponent<CinemachineBrain>();
     }
 
     private void LateUpdate()
     {
-        if(!inDungeon && _playerTransform)
+        if (!inDungeon && _playerTransform && !isShaking)
         {
-           Vector3 targetPosition = new(_playerTransform.position.x, _playerTransform.position.y, -100f);
-           transform.position = Vector3.Lerp(transform.position, targetPosition, 5f * Time.deltaTime);
+            //Vector3 targetPosition = new(_playerTransform.position.x, _playerTransform.position.y, -100f);
+            //transform.position = Vector3.Lerp(transform.position, targetPosition, 5f * Time.deltaTime);
+            brain.enabled = true;
+        }
+        else if (inDungeon && !isShaking)
+        {
+            brain.enabled = false;
         }
     }
 
+    public void changeCameraShakeEnabled()
+    {
+        cameraShakeEnabled = !cameraShakeEnabled;
+    }
+
+    [ContextMenu("Test Camera Shake")]
+    public void testCameraShake()
+    {
+        if(cameraShakeEnabled)
+            StartCoroutine(cameraShake());
+    }
+
+    public IEnumerator cameraShake()
+    {
+        isShaking = true;
+        brain.enabled = false;
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < cameraShakeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = cameraShakeCurve.Evaluate(elapsedTime / cameraShakeDuration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+        brain.enabled = true;
+    }
     public IEnumerator UpdatePosition(Vector3 targetPosition, System.Action callback = null)
     {
         Dungeon_Controller.instance.isTransitioning = true;
